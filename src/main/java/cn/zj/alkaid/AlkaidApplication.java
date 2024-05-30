c126ac7ebb51e077b044b3c0176018a70dfb4074
zhangjian <zj.dalian.cn@gmail.com>
2024-05-30 21:49:01 +0800

    init

---
+++ src/main/java/cn/zj/alkaid/AlkaidApplication.java
@@ -0,0 +1,13 @@
+package cn.zj.alkaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlkaidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlkaidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/zj/alkaid/AlkaidApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zj.alkaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkaidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
